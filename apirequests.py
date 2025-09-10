6fcf56ade097c1a44d9993f18964e58af9d8f603
kaaylooon <kaylon.alt@outlook.com>
2025-09-10 20:49:02 -0300

    update

--- apirequests.py
+++ apirequests.py
@@ -5,21 +5,33 @@ CEP_URL = "https://cep.awesomeapi.com.br/"
 GEOAPI_URL = "https://nominatim.openstreetmap.org/search"
 
 def get_coordenadas(address):
-	params = {"q": address, "format": "json"}
-	headers = {"User-Agent": "AppTest/1.0 (kaylon.alt@outlook.com)"}
-	try:
-		response = requests.get(GEOAPI_URL, params=params, headers=headers, timeout=5)
-		response.raise_for_status()  # levanta erro se status != 200
-		data = response.json()
-		if data:
-			lat = float(data[0]["lat"])
-			lon = float(data[0]["lon"])
-			return lat, lon
-		else:
-			address = 'Rua Planalto, 205, Centro, Macajuba, BA'
-	except Exception as e:
-		print("Erro ao obter coordenadas:", e)
-		address = 'Rua Planalto, 205, Centro, Macajuba, BA'
+    params = {"q": address, "format": "json"}
+    headers = {"User-Agent": "AppTest/1.0 (kaylon.alt@outlook.com)"}
+    try:
+        response = requests.get(GEOAPI_URL, params=params, headers=headers, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+        if data:
+            lat = float(data[0]["lat"])
+            lon = float(data[0]["lon"])
+            return lat, lon
+    except Exception as e:
+        print("Erro ao obter coordenadas:", e)
+
+    fallback_address = 'Rua Planalto, 205, Centro, Macajuba, BA'
+    
+    try:
+        response = requests.get(GEOAPI_URL, params={"q": fallback_address, "format": "json"}, headers=headers, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+        if data:
+            lat = float(data[0]["lat"])
+            lon = float(data[0]["lon"])
+            return lat, lon
+    except Exception as e:
+        print("Erro ao obter coordenadas do fallback:", e)
+
+    return None, None
 
 def get_munipicios(uf):
 	url = f"{ESTADO_URL}/{uf}/municipios"
